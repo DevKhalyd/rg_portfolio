844969bebeb28a9908f752404a8d0b90fa4d474f
Rolando Garcia <jrdosmil0@gmail.com>
2022-01-15 20:37:33 -0600

    Header Searcher

--- lib/features/search/presentation/widgets/body/body_searcher.dart
+++ lib/features/search/presentation/widgets/body/body_searcher.dart
@@ -1,10 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../../../../../core/extensions/build_context_ext.dart';
+import '../../../../home/presentation/bloc/home_bloc.dart';
 
 class BodySearcher extends StatelessWidget {
   const BodySearcher({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Expanded(
+      child: Stack(
+        children: [
+          BlocBuilder<HomeBloc, HomeState>(builder: (_, state) {
+            if (state is HomeToggleMenu) {
+              return state.isOpen
+                  ? Positioned(
+                      top: context.getPercentHeight(0.0005),
+                      right: context.getPercentWidth(0.015),
+                      child: state.menu,
+                    )
+                  : const SizedBox();
+            }
+            return const SizedBox();
+          }),
+        ],
+      ),
+    );
   }
 }
--- lib/features/search/presentation/widgets/header/header_searcher.dart
+++ lib/features/search/presentation/widgets/header/header_searcher.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:rg_portfolio/features/home/presentation/widgets/body/txt_field_searcher.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:rg_portfolio/core/menus/menu_about_me.dart';
 
 import '../../../../../core/extensions/build_context_ext.dart';
+import '../../../../../core/menus/menu_options.dart';
 import '../../../../../core/widgets/profile_icon.dart';
+import '../../../../home/presentation/bloc/home_bloc.dart';
+import '../../../../home/presentation/widgets/body/txt_field_searcher.dart';
 import '../../../../home/presentation/widgets/header/icon_menu.dart';
 import '../../../../home/presentation/widgets/shared/rg_name.dart';
 
@@ -12,7 +16,10 @@ class HeaderSearcher extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: EdgeInsets.only(top: context.getPercentHeight(0.015)),
+      padding: EdgeInsets.only(
+        top: context.getPercentHeight(0.015),
+        bottom: context.getPercentHeight(0.01),
+      ),
       child: Row(
         children: [
           SizedBox(width: context.getPercentWidth(0.01)),
@@ -24,10 +31,36 @@ class HeaderSearcher extends StatelessWidget {
           const TextFieldSearcher(),
           const Spacer(),
           IconMenu(
-            onPressed: () {},
+            onPressed: () {
+              if (context.isMobileSize) {
+                showDialog(
+                  context: context,
+                  builder: (_) => const Dialog(
+                    child: MenuOptions(),
+                  ),
+                );
+                return;
+              }
+              context
+                  .read<HomeBloc>()
+                  .add(const HomeTogglePressed(menu: MenuOptions()));
+            },
           ),
           ProfileIcon(
-            onPressed: () {},
+            onPressed: () {
+              if (context.isMobileSize) {
+                showDialog(
+                  context: context,
+                  builder: (_) => const Dialog(
+                    child: MenuAboutMe(),
+                  ),
+                );
+                return;
+              }
+              context
+                  .read<HomeBloc>()
+                  .add(const HomeTogglePressed(menu: MenuAboutMe()));
+            },
           ),
         ],
       ),
